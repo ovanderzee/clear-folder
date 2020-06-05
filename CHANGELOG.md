1b2ee1dc72cb0a51ee36ac3b0777a91a3b131baf
ovanderzee <hastalavista@dds.nl>
2020-06-05 16:05:12 +0200

    changelog and documentation

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# Changelog
+
+## [2.0.0] - 2020-06-05
+
+After a year it was time to mature clear-folder a bit.
+The main intention is the scripts section of the package.json,
+still bare-bones.
+
+### Added
+- Brief alias 'cf' for the command name 'clear-folder'
+- Errorhandling and error messages
+- Tests
+- Formatting
+
+### Changed
+- Path operators ( '.', '..', '~' ) are rejected now.
--- cli.js
+++ cli.js
@@ -2,6 +2,10 @@
 
 const fs = require('fs')
 
+/**
+ * Remove from deepest subfolder up to specified folder
+ * @param {string} path - folder name
+ */
 const clearFolderContents = function ( path ) {
     if ( fs.existsSync( path ) ) {
         fs.readdirSync(path).forEach ( function ( file, index ) {
@@ -18,11 +22,18 @@ const clearFolderContents = function ( path ) {
     }
 }
 
+/**
+ * Check input sanity
+ * @param {string[]} folder - folder names relative to working directory
+ */
 const clearFolderGate = function ( folders ) {
+    // usage hint
     if ( !folders.length || ['--help', '-h', '-?'].includes(folders[0])) {
-      throw new Error(`clear-folder needs one or more foldernames to operate`)
+        throw new Error(`clear-folder needs one or more foldernames to operate`)
     }
 
+    // do not operate when finding opportunities to work
+    // other than below the working directory
     folders.forEach ( function ( folder ) {
         const folderArray = folder.split('/')
         if (
@@ -34,9 +45,14 @@ const clearFolderGate = function ( folders ) {
                 `clear-folder can't work with path operators in ${folder}`
             )
         }
+    })
+
+    folders.forEach ( function ( folder ) {
         try {
             clearFolderContents( process.cwd() + '/' + folder )
         } catch ( err ) {
+            // catch find-folder errors,
+            // the command will be used fot hthe sake of being sure
             console.error( `${err.name}: ${err.message}` )
         }
     })
